f261ed8bca496c89fde50720e23ebbf7f5a0fcbd
Patschenkof <Robin.Fehling@googlemail.com>
2023-10-31 13:35:01 +0100

    added tf.compat.v1 in line 123

--- neuralgym/ops/layers.py
+++ neuralgym/ops/layers.py
@@ -120,7 +120,7 @@ def resize(x, scale=2, to_shape=None, align_corners=True, dynamic=False,
     else:
         xs = x.get_shape().as_list()
         new_xs = [int(xs[1]*scale), int(xs[2]*scale)]
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
         if to_shape is None:
             x = func(x, new_xs, align_corners=align_corners)
         else:
